66e12545b39f1e63999912d2b8d582aee94bd240
blurkness <blurkness@gmail.com>
2016-07-19 17:54:23 -0300

    Tentando outro componente

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,21 +1,24 @@
-System.register('beeta-dev/ext-userinfo/main', ['flarum/extend', 'flarum/app', 'flarum/components/WelcomeHero'], function (_export) {
+System.register('beeta-dev/ext-userinfo/main', ['flarum/extend', 'flarum/app', 'flarum/components/IndexPage'], function (_export) {
     'use strict';
 
-    var extend, app, WelcomeHero;
+    var extend, app, IndexPage;
     return {
         setters: [function (_flarumExtend) {
             extend = _flarumExtend.extend;
         }, function (_flarumApp) {
             app = _flarumApp['default'];
-        }, function (_flarumComponentsWelcomeHero) {
-            WelcomeHero = _flarumComponentsWelcomeHero['default'];
+        }, function (_flarumComponentsIndexPage) {
+            IndexPage = _flarumComponentsIndexPage['default'];
         }],
         execute: function () {
 
             app.initializers.add('beeta-dev-ext-ads', function () {
-                extend(WelcomeHero.prototype, 'init', function () {
+                extend(IndexPage.prototype, 'init', function () {
                     var ads = '<li class="TagTile colored" style="background-color: #14191f;"><a class="TagTile-info" href="#"><h3 class="TagTile-name">Ads</h3><p class="TagTile-description"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- Beeta 2.0 --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3763973333557675" data-ad-slot="1731508684" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></p><div class="TagTile-children"></div></a><span class="TagTile-lastDiscussion"></span></li>';
-                    $(".TagsPage-content .TagTiles").append(ads);
+                    setTimeout(function () {
+                        $(".TagsPage-content .TagTiles").append(ads);
+                        console.log(ads);
+                    }, 3000);
                 });
             });
         }
