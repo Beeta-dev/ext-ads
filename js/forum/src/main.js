7567401ac12e2c712ba7bd3734cc9bf67101c65f
blurkness <blurkness@gmail.com>
2016-07-19 18:12:32 -0300

    Esquece de configurar o gulp, da nisso

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -3,7 +3,7 @@ import app from 'flarum/app';
 import Page from 'flarum/components/Page'
 
 app.initializers.add('beeta-dev-ext-ads', function() {
-    extend(IndexPage.prototype, 'init', function() {
+    extend(Page.prototype, 'init', function() {
         var ads = '<li class="TagTile colored" style="background-color: #14191f;"><a class="TagTile-info" href="#"><h3 class="TagTile-name">Ads</h3><p class="TagTile-description"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- Beeta 2.0 --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3763973333557675" data-ad-slot="1731508684" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></p><div class="TagTile-children"></div></a><span class="TagTile-lastDiscussion"></span></li>';
         setTimeout(function(){
             $(".TagsPage-content .TagTiles").append(ads);
