1c7db44062d15df05794e72074e16858aea655f3
blurkness <blurkness@gmail.com>
2016-07-19 21:00:22 -0300

    Sup

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,7 +1,7 @@
-System.register('beeta-dev/ext-ads/main', ['flarum/extend', 'flarum/app', 'flarum/components/WelcomeHero', 'flarum/components/HeaderPrimary'], function (_export) {
+System.register('beeta-dev/ext-ads/main', ['flarum/extend', 'flarum/app', 'flarum/components/WelcomeHero', 'flarum/components/DiscussionHero', 'flarum/components/HeaderPrimary'], function (_export) {
     'use strict';
 
-    var extend, app, WelcomeHero, HeaderPrimary;
+    var extend, app, WelcomeHero, DiscussionHero, HeaderPrimary;
     return {
         setters: [function (_flarumExtend) {
             extend = _flarumExtend.extend;
@@ -9,6 +9,8 @@ System.register('beeta-dev/ext-ads/main', ['flarum/extend', 'flarum/app', 'flaru
             app = _flarumApp['default'];
         }, function (_flarumComponentsWelcomeHero) {
             WelcomeHero = _flarumComponentsWelcomeHero['default'];
+        }, function (_flarumComponentsDiscussionHero) {
+            DiscussionHero = _flarumComponentsDiscussionHero['default'];
         }, function (_flarumComponentsHeaderPrimary) {
             HeaderPrimary = _flarumComponentsHeaderPrimary['default'];
         }],
@@ -28,7 +30,19 @@ System.register('beeta-dev/ext-ads/main', ['flarum/extend', 'flarum/app', 'flaru
                         if ($(".UserPage").html() == undefined) {
                             $(".Hero .container").prepend(ads);
                         }
-                        if ($(".TagsPage-nav").html() == undefined) {
+                        if ($(".TagsPage-nav").html() == undefined && $(".UserPage-nav").html() == undefined) {
+                            $(".sideNav").append(ads);
+                        }
+                    }, 300);
+                });
+                extend(DiscussionHero.prototype, 'init', function () {
+                    var ads = '<div class="ads1" style="width:100%;text-align:center;margin: 0 0 10px 0;"><!-- Beeta 2.0 --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3763973333557675" data-ad-slot="1731508684" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>';
+                    $(".Hero .container").prepend(ads);
+                    setTimeout(function () {
+                        if ($(".UserPage").html() == undefined) {
+                            $(".Hero .container").prepend(ads);
+                        }
+                        if ($(".TagsPage-nav").html() == undefined && $(".UserPage-nav").html() == undefined) {
                             $(".sideNav").append(ads);
                         }
                     }, 300);
